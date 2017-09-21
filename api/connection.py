243b7c9941f76caefcefa4db538fbf70dafc52f9
unknown <olopez@pympack.com.pe>
2017-09-21 09:51:52 -0500

    integracion generator tables

--- api/connection.py
+++ api/connection.py
@@ -4,7 +4,7 @@ import logging
 import pdb #testing
 
 class api:
-    _url = 'http://localhost:8001/'
+    _url = 'http://localhost:3000/'
     _client_id = 'FUN5EkBDdiQo8SixJYslF41ZkrjAhQtWKkV7BEE2'
     _client_secret = 'P7pj3QcE6GyhlS0QUzJOJ8SRLlTGHYSMESGr7QzlyKnuMsc4osaaStB6wSiGGxAelKjXv6xJ7p9Go8R5el2KaVlcM4P50IeplkY2CWHxaOc9NI1fLKS9DoWxD9UbG4TN'
     _grant_type = 'password'
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,11 +3,28 @@ from django.urls import reverse
 from django.http import HttpResponse
 from django.views import View
 from api.connection import api
+from django.utils.translation import ugettext_lazy as _
+
+from api.json2table import convert
+
+
 
 class Specialist:
 
     def showList(request):
-        return render(request, 'admin/actor/specialistList.html')
+        ObjApi = api()
+        json_object = ObjApi.get('clients/')
+    
+        #json_object = {"key" : "value","key2" : "value2","key3" : "value3","key4" : "value4"}
+        
+        customColumn = {"last_name":{'username','last_name'}}
+
+        header = {"Lastname":"last_name","Code":"code","Email":"email_exact","RUC":"ruc","Category":"institute","Specialty":"nationality"}
+        table_attributes = {"class" : "table table-striped table-bordered table-hover"}
+
+        data = convert(json_object, table_attributes=table_attributes,header=header, customColumn=customColumn)
+
+        return render(request, 'admin/actor/specialistList.html',{'data': data})
 
     def showSpecialistProfile(request,client_id):        
         ObjApi = api()
