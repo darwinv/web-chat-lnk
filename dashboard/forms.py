acaa4e96784e3b072615c1a6f553c6aef3fbdddc
oscarlopezdeveloper <olopez@pympack.com.pe>
2017-12-05 09:56:46 -0500

    ruc requerido y no requerido specialista

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,7 +1,7 @@
 
 from django import forms
 from django.forms import ModelForm
-from api.models import Specialist, Category, Department, Province, District
+from api.models import Specialist, Category, Department, Province, District, Countries
 from django.utils.translation import ugettext_lazy as _
 from api.connection import api
 
@@ -49,27 +49,24 @@ class SellerFormFilters(FilterForm):
     count_queries_seller = forms.IntegerField(label = cap(_('number of queries sold greater than')))
 
 
-class SpecialistForm(ModelForm):   
+class SpecialistForm(ModelForm):
 
     category = forms.CharField(widget=forms.Select(), required=True, label = cap(_('category')))
-    department = forms.CharField(widget=forms.Select(), required=True, label = cap(_('department'))  )
-    province = forms.CharField(widget=forms.Select(), required=True, label = cap(_('province'))  )
-    district = forms.CharField(widget=forms.Select(), required=True, label = cap(_('district'))  )
-    street = forms.CharField(required=True, label = cap(_('street')))
+    department = forms.CharField(widget=forms.Select(), required=False, label = cap(_('department')))
+    province = forms.CharField(widget=forms.Select(), required=False, label = cap(_('province')))
+    district = forms.CharField(widget=forms.Select(), required=False, label = cap(_('district')))
+    street = forms.CharField(required=False, label = cap(_('street')))
     photo = forms.FileField(required=False, label = cap(_('upload a photo')), widget=forms.TextInput(
         attrs={'class': 'sr-only inputFile', 'id': 'inputFile', 'accept': '.jpg,.jpeg,.png,.gif,.bmp,.tiff', 'type': 'file'}, ))
     img_document_number = forms.FileField(required=False, label = cap(_('upload document')), widget=forms.TextInput(
         attrs={'class': 'sr-only inputFile', 'accept': '.jpg,.jpeg,.png,.gif,.bmp,.tiff', 'type': 'file', 'data-title':'True'}, ))
 
-
-
-
     username = forms.CharField(label = cap(_('username')))
     email_exact = forms.CharField(label = cap(_('email')))
     document_number = forms.CharField(label = cap(_('document number')))
     ruc = forms.CharField(label = cap(_('RUC')))
-
-
+    nationality = forms.CharField(widget=forms.Select(), required=True, label=cap(_('nationality')))
+    residence_country = forms.CharField(widget=forms.Select(), required=True, label=cap(_('residence country')))
 
     def __init__(self, initial=None, department=None, province=None, form_edit=None,
                  *args, **kwargs):
@@ -77,12 +74,19 @@ class SpecialistForm(ModelForm):
 
         categories = Category.objects.all()
         departments = Department.objects.all()
+        countries = Countries.objects.all()
 
         if categories:
             self.fields['category'].widget.choices = [('', '')] + [(l.id, _(l.name)) for l in categories]
 
         if departments:
             self.fields['department'].widget.choices = [('', '')] + [(l.id, _(l.name)) for l in departments]
+
+        if countries:
+            self.fields['nationality'].widget.choices = [('', '')] + [(l.id, _(l.name)) for l in countries]
+
+        if countries:
+            self.fields['residence_country'].widget.choices = [('', '')] + [(l.id, _(l.name)) for l in countries]
 
         if department:
             provinces = Province.objects.filter(department_id=department)
@@ -148,6 +152,50 @@ class SpecialistForm(ModelForm):
             'payment_per_answer': cap(_('payment per answer')),
         }
 
+
+    def clena_ruc(self):
+        data = self.cleaned_data['ruc']
+        country = self.cleaned_data['residence_country']
+        if country=='1' and not data:
+            raise forms.ValidationError(_("required"))
+
+        return data
+
+    
+    def clena_department(self):
+        data = self.cleaned_data['department']
+        country = self.cleaned_data['residence_country']
+        if country=='1' and not data:
+            raise forms.ValidationError(_("required"))
+
+        return data
+
+    
+    def clena_province(self):
+        data = self.cleaned_data['province']
+        country = self.cleaned_data['residence_country']
+        if country=='1' and not data:
+            raise forms.ValidationError(_("required"))
+
+        return data
+
+    
+    def clena_district(self):
+        data = self.cleaned_data['district']
+        country = self.cleaned_data['residence_country']
+        if country=='1' and not data:
+            raise forms.ValidationError(_("required"))
+
+        return data
+
+    
+    def clena_street(self):
+        data = self.cleaned_data['street']
+        country = self.cleaned_data['residence_country']
+        if country=='1' and not data:
+            raise forms.ValidationError(_("required"))
+
+        return data
 
 """
 Reportes de estado de cuenta
