4a93fefe95062f1f030c2cc7417df73363ddd079
oscarlopezdeveloper <olopez@pympack.com.pe>
2018-03-16 14:38:23 -0500

    arquitectura chat de usuario

---
+++ EjemploProtocolos-20180316T191232Z-001/EjemploProtocolos/EjemploProtocolos/Constantes.swift
@@ -0,0 +1,12 @@
+//
+//  Constantes.swift
+//  EjemploProtocolos
+//
+//  Created by Christian Quicano on 10/26/17.
+//  Copyright © 2017 Christian Quicano. All rights reserved.
+//
+
+import Foundation
+
+let kTelefono = "telefono"
+let kStatus = "status"
