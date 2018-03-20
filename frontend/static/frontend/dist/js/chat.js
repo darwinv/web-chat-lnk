652b462a964903087a1628c656d14ce71a4528ce
oscarlopezdeveloper <olopez@pympack.com.pe>
2018-03-20 15:43:09 -0500

    title socket

--- frontend/static/frontend/dist/js/chat.js
+++ frontend/static/frontend/dist/js/chat.js
@@ -3,7 +3,7 @@ $(function() {
 changeMessage(); //se llama a la funcion change message
 
 function changeMessage(){
-    var query_id = null
+    var previus_query_id = null
     $(".message").each(function(){
         var msg = $(this);
         var user_id = userID
@@ -13,53 +13,59 @@ function changeMessage(){
             var bloq = msg.parent()
         }
 
-        if (msg.data("query") != query_id){
+        if (msg.data("query") != previus_query_id){
             msg.siblings(".cont-title-query").show()
-            query_id = msg.data("query");
+            previus_query_id = msg.data("query");
         }else{
             msg.siblings(".cont-title-query").remove()
         }
+
     });
 }
 
 
-var ws_scheme = window.location.protocol == "http:" ? "wss" : "ws";
-//conformamos la url para conectar via ws
-api_url = apiUrl.replace("http","ws");
-// Comparamos los roles para que se conecten a la sala como corresponde
-role_id = roleID;
-var cadena = window.location.pathname.split("/");
- if (role_id == 2){
-     //extraemos el id actual de usuario
-     var user_id = userID
-     //extraemos la categoria
-     var category = cadena[5];
- }
- else{
-     var user_id = cadena[5];
-     var category = $('.message').data("category");
- }
- // Nuestra sala, sera id usuario y id de especialidad
- var sala = user_id + '-' + category;
- var chatsock = new ReconnectingWebSocket(api_url + "/chat" + "/" + sala);
+    var ws_scheme = window.location.protocol == "http:" ? "wss" : "ws";
+    //conformamos la url para conectar via ws
+    api_url = apiUrl.replace("http","ws");
+    // Comparamos los roles para que se conecten a la sala como corresponde
+    role_id = roleID;
+    var cadena = window.location.pathname.split("/");
+    if (role_id == 2){
+        //extraemos el id actual de usuario
+        var user_id = userID
+        //extraemos la categoria
+        var category = cadena[5];
+    }
+    else{
+        var user_id = cadena[5];
+        var category = $('.message').data("category");
+    }
+    // Nuestra sala, sera id usuario y id de especialidad
+    var sala = user_id + '-' + category;
+    console.log(sala)
+    var chatsock = new ReconnectingWebSocket(api_url + "/chat" + "/" + sala);
 
-  chatsock.onopen = function open() {
-  console.log('WebSockets connection created.');
-};
+    chatsock.onopen = function open() {
+        console.log('WebSockets connection created.');
+    };
 
- chatsock.onmessage = function(message) {
-     var data = JSON.parse(message.data);
-     // alert(message.data)
-     console.log(data)
-     var box_chat = $("#chat_box");
-     $.each(data, function(key,value){
-         var msg = value.message;
-         var time = value.timeMessage;
-         var codeUser = value.codeUser;
+chatsock.onmessage = function(message) {
+    var data = JSON.parse(message.data);
+    // alert(message.data)
+    var box_chat = $("#chat_box");
+    $.each(data, function(key,value){
+        var msg = value.message;
+        var time = value.timeMessage;
+        var codeUser = value.codeUser;
 
-// Se crea el div del globo para renderizarlo
-// debe validarse el tema de si soy el q cree el mensaje o al contrario
-         var divMessage = "<div class='row globe-chat'>"+
+        // Se crea el div del globo para renderizarlo
+        // debe validarse el tema de si soy el q cree el mensaje o al contrario
+        var divMessage =   "<div class='row globe-chat'>"+
+                                "<div class='cont-title-query' style='display: none'>"+
+                                    "<div class='title-query'>"+
+                                        value.query.title+
+                                    "</div>"+
+                                "</div>"+
                                 "<div class='message col-sm-6 col-sm-offset-6'"+
                                 "data-sender='"+value.user_id+"' data-query='"+value.query.id+"'>"+
                                     "<div class='row'>"+
@@ -83,14 +89,13 @@ var cadena = window.location.pathname.split("/");
     if (!$("#animacion").hasClass('hidden')){
         $("#animacion").addClass("hidden");
     }
- };
+};
 
-
- $("#send-query").on("click", function(event) {
-     $("#animacion").toggleClass("hidden")
-     message_type = 'q';
-     title_query = $('#title_query').val();
-     query_id = "";
+$("#send-query").on("click", function(event) {
+    $("#animacion").toggleClass("hidden")
+    message_type = 'q';
+    title_query = $('#title_query').val();
+    query_id = "";
     if (role_id == 3) {
         message_type = 'a';
         title_query = "";
@@ -98,27 +103,27 @@ var cadena = window.location.pathname.split("/");
         query_id = $("#chat_box div.message:last").data("query");
     }
     console.log(query_id)
-     var message = {
-         token : token,
-         title: title_query,
-         message:[{
+    var message = {
+        token : token,
+        title: title_query,
+        message:[{
             message: $('#text_message').val(),
             msg_type: message_type,
             content_type: "0",
             file_url: ""}],
-         category: category,
-         query: query_id
-     }
+        category: category,
+        query: query_id
+    }
 
-     chatsock.send(JSON.stringify(message));
+    chatsock.send(JSON.stringify(message));
 
-     $("#title_query").val('')
-     $("#text_message").val('').focus();
+    $("#title_query").val('')
+    $("#text_message").val('').focus();
 
-     return false;
- });
+    return false;
+});
 
- if (chatsock.readyState == WebSocket.OPEN) {
-  chatsock.onopen();
+if (chatsock.readyState == WebSocket.OPEN) {
+    chatsock.onopen();
 }
 });
