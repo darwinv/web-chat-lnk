8c4e7a162f6835a7bbff7244f7011f34e2b7c2e6
darwinv <darwin.vasz@gmail.com>
2018-04-16 11:05:56 -0500

    se sincroniza con la web

--- frontend/static/frontend/client/js/categories-ref.js
+++ frontend/static/frontend/client/js/categories-ref.js
@@ -1,61 +1,36 @@
 $(document).ready(function () {
-
-//     $(".e").on("click", function(){
-//         console.log("pasee1");
-// }); $(".e img").on("click", function(){
-//         console.log("pasee2");
-// }); $(".cont-item").on("click", function(){
-//         console.log("pasee3");
-// }); $(".itemp").on("click", function(){
-//         console.log("pasee4");
-// }); $(".info-div").on("click", function(){
-//         console.log("pasee5");
-// });
-
+    // Cargamos Firebase
     firebase.initializeApp(JSON.parse(apiEnvFirebase));
     console.log(apiEnvFirebase);
-    // firebase.auth().signInAnonymously().catch(function (error) {
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     // ...
-    // });
     var client_id = userID;
     console.log("id: "+client_id);
-    // if (!client_id == "None") {
-        var starCountRef = firebase.database().ref('categories/clients/u' + client_id).orderByChild('datetime');
-        starCountRef.on('value', function (snapshot) {
+    // cargamos el listado de los mensajes de las especialidades
+    var starCountRef = firebase.database().ref('categories/clients/u' + client_id).orderByChild('datetime');
+    starCountRef.on('value', function (snapshot) {
             $("#list_categories").empty();
-            // $("#lista_cats").remove();
             var id_user = snapshot.key;
-            // console.log(id_user);
             inject_items(reverse_list(snapshot),id_user);
-            // snapshot.forEach(function (item) {
-            //     var itemVal = item.val();
-            //     if ($("#cat" + item.key).length) {
-            //         $("#cat" + item.key).replaceWith("<a href='#' class='list-group-item justify-content-between list-group-item-action' id='" + "cat" + item.key + "'>" + "<img src='" + itemVal.image + "' class='rounded-circle'>" + itemVal.name + "<img src='https://s3.amazonaws.com/linkup-photos/82cffad1-e6f8-4f75-a711-a33c3f272131.png' class=''></a>");
-            //     }
-            //     else {
-            //         $("#list_categories").append("\
-            //                 <a href='#' class='list-group-item list-group-item-action' id='" + "cat" + item.key + "'>\
-            //                     <div class='row'>\
-            //                         <div class='col-4'>\
-            //                             <img src='" + itemVal.image + "'\
-            //                                  class='rounded-circle' id='img_cat'>\
-            //                         </div>\
-            //                         <div class='col-4' id=''>\
-            //                             <span>" + itemVal.name + "</span>\
-            //                         </div>\
-            //                         <div class='col-2 offset-2'>\
-            //                             <img src='https://s3.amazonaws.com/linkup-photos/82cffad1-e6f8-4f75-a711-a33c3f272131.png' class=''>\
-            //                         </div>\
-            //                     </div>\
-            //                 </a>");
-            //         // $("#list_categories").append("<a href='#' class='list-group-item justify-content-between list-group-item-action' id='" + "cat" + item.key + "'>" + "<img src='" + itemVal.image + "' class='rounded-circle'>" + itemVal.name + "<img src='https://s3.amazonaws.com/linkup-photos/82cffad1-e6f8-4f75-a711-a33c3f272131.png' class=''></a>");
-            //     }
-            // });
         });
-    // }
-});
+
+   // cargamos el plan elegido
+    var planObject = firebase.database().ref('chosenPlans/u' + client_id);
+    planObject.on('value', function (snap){
+      var plan = snap.val().plan_name;
+      var available_queries = snap.val().available_queries;
+      var query_quantity = snap.val().query_quantity;
+      var expiration_date = snap.val().expiration_date;
+      $("#chosen-plan").data("id", snap.val().id);
+      $("#plan_name").text(plan);
+      $("#queries").text(available_queries + '/'+ query_quantity);
+      $("#expiration_date").text(expiration_date);
+
+      // console.log(snap.val());
+    });
+}); // cierra document ready
+
+
+
+
 function putViewToApi(p_1, p_2) {
     console.log(p_1,p_2);
     $.ajax({
