bbc2618681c9f79f23aa10e3446db3fc1dbe8eee
oscarlopezdeveloper <olopez@pympack.com.pe>
2018-10-19 13:05:50 -0500

    back status multi

--- api/connection.py
+++ api/connection.py
@@ -205,12 +205,12 @@ class api:
             return True
 
 
-    def get_all(self, token, slug='', arg=None):
+    def get_all(self, token, slug='', arg=None, data=None):
 
         try:
             headers = {'Authorization': 'Bearer ' + token}
             headers = dict(headers, **self._headers)
-            r = requests.get(self._url + slug, headers=headers, params=arg)
+            r = requests.get(self._url + slug, headers=headers, params=arg, data=data)
             return r
 
         except Exception as e:
