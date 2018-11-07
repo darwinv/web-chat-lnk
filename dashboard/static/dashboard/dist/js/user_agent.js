b7d6457ed175247dc26414ba16c07fa1941236e2
darwinv <darwin.vasqz@gmail.com>
2018-11-07 12:27:12 -0500

    agregado detectar y mostrar dialogo en ios

--- dashboard/static/dashboard/dist/js/user_agent.js
+++ dashboard/static/dashboard/dist/js/user_agent.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
 
 function getMobileOperatingSystem() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  
+
         // Windows Phone must come first because its UA also contains "Android"
       if (/windows phone/i.test(userAgent)) {
           console.log("Windows Phone");
       }
-  
+
       if (/android/i.test(userAgent)) {
           if (confirm("¿Deseas usar la app?")) {
           console.log("android");
@@ -16,12 +16,13 @@ function getMobileOperatingSystem() {
           window.location.href = play_store;
           }
       }
-  
+
       // iOS detection from: http://stackoverflow.com/a/9039885/177710
       if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-          console.log("iOS");
+          var url_link = "https://testflight.apple.com/join/YwSCtt0f";
+          window.location.href = url_link;
       }
-  
+
       console.log(userAgent);
   }
   getMobileOperatingSystem();
