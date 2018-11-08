33cd835284bacc4a395918bdc1b3f2c6fed15326
darwinv <darwin.vasqz@gmail.com>
2018-11-08 11:48:46 -0500

    agregada clases y condiciones para el manejo de varios estados

--- frontend/static/frontend/client/js/footer_plan_ref.js
+++ frontend/static/frontend/client/js/footer_plan_ref.js
@@ -6,35 +6,59 @@ $(document).ready(function () {
   planObject = firebase.database().ref('chosenPlans/u' + client_id);
 
 
-  $.get( url_status_plan, function( data ) {
 
-});
 
 //
 // $('#reload').on('hidden.bs.modal', function () {
  planObject.on('value', function (snap){
-     if (snap.exists()) {
-       var available_queries = snap.val().available_queries;
-       var query_quantity = snap.val().query_quantity;
-       var expiration_date = snap.val().expiration_date;
-       text = `<h4>${TRANS_QUERY_PLANS}</h4>
-       <p id="plan_name">${snap.val().plan_name}</p>
-       <p>${TRANS_AVAILABLES}:
-         <span id="queries">${available_queries} / ${query_quantity}</span>
-       </p>
-       <p>${TRANS_EXPIRATION_DATE}:
-         <span id="expiration_date">${expiration_date}</span></p><center>
-         <button id="change_plan" type="button" class="btn btn-xs btn-ligth-blue cap"
-        data-toggle="modal" data-target="#changePlan">${TRANS_CHANGE_PLAN}
-        </button> </center> `;
-         $("#chosen-plan").html(text);
-     }
-     else{
-       $("#chosen-plan").html(
-         "<center> <button type='button' class='btn btn-md btn-ligth-blue cap' data-toggle='modal'"+
-           "data-target='#reload'>"+ TRANS_RELOAD +"</button> </center>");
-       }
-  });
+    $.get( url_status_plan, function( data ) {
+        console.log(data); 
+      
+      if (snap.exists()) {
+        
+          var available_queries = snap.val().available_queries;
+          var query_quantity = snap.val().query_quantity;
+          var expiration_date = snap.val().expiration_date;
+          
+          text1 = `<h4>${TRANS_QUERY_PLANS}</h4>
+                  <p id="plan_name">${snap.val().plan_name}</p>
+                  <p>${TRANS_AVAILABLES}:
+                  <span id="queries">${available_queries} / ${query_quantity}</span></p>
+                  <p>${TRANS_EXPIRATION_DATE}:
+                  <span id="expiration_date">${expiration_date}</span></p>`;
+          
+          textButton = `<center> <button id="change_plan" type="button" class="btn btn-xs btn-ligth-blue cap"
+                        data-toggle="modal" data-target="#changePlan">${TRANS_CHANGE_PLAN}</button> 
+                        </center>`;
+          
+          finalText = text1 + textButton;              
+          if(data.code == 5) {
+            textExpired = `<p class='expired-plan'> Su plan Seleccionado ha expirado</p>`;
+            finalText = text1 + textExpired + textButton;
+          }  
+
+          $("#chosen-plan").html(finalText);
+      }
+      else{
+          selectPlanBtn = `<center><button id="change_plan" type="button" class="btn btn-xs btn-ligth-blue cap"
+                            data-toggle="modal" data-target="#changePlan">${TRANS_CHANGE_PLAN}
+                            </button>`;
+
+              if (data.code == 3) {
+                  $("#chosen-plan").html(selectPlanBtn);
+              }
+              if(data.code == 2) {
+                 $("#chosen-plan").html(`<div> No tienes un plan de consumo activo </div>`);       
+                 $("#chosen-plan").addClass("no-activeplan");
+              }
+                if (data.code == 1) {
+                    $("#chosen-plan").html(
+                    "<center> <button type='button' class='btn btn-md btn-ligth-blue cap' data-toggle='modal'"+
+                    "data-target='#reload'>"+ TRANS_RELOAD +"</button> </center>");
+                }
+          }
+    });
+});  
    $("#chosen-plan").toggleClass('hidden');
 // });
 
