e8f442783e1bf83a29e6ade958a57dcc1cee26f8
darwinv <darwin.vasqz@gmail.com>
2018-11-12 17:52:00 -0500

    css acomodado para que pueda obtener estilo

--- frontend/static/frontend/dist/js/match.js
+++ frontend/static/frontend/dist/js/match.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    $('#list-match-content').data("url",'client/matchs/');  // Url a consumir
+    $('#list-match-content').data("page", 1);
     $(".match").each(function(){
         // Renderizamos el time en el listado
         // le damos  formato a de fecha a todas las fechas de match
@@ -11,10 +13,36 @@ $(document).ready(function () {
         }
     });
 
-    var win = $("#list-match-content");
-    win.data("url",'client/matchs/');  // Url a consumir
-    win.data("page", 1);  // Inicializacion de pagina en 0, solo Modales
-    win.sendAjaxPagination(function(resp){
-        console.log(resp);
-    });
+
+    $('#list-match-content').scroll(function() {
+        // Cuando se realiza Scroll sobre el listado
+        win = $('#list-match-content');
+        if (win.canMakeScrollPagination()){
+            
+          loadMatchs(win);
+        }
+      });
+
+
+    function loadMatchs(win){
+        win.sendAjaxPagination(function(data) {
+          showMatchs(data.results)
+        });
+      }
+
+      function showMatchs(dataList){
+        // maquetar listado de planes segun objeto iterable
+        $.each(dataList, function(i,v){
+          var html = "<input name='active_plans' value='"+v.id+"' type='radio'>";
+          var text = `<label class='font-size25 marL5 marB-5'> ${v.plan_name} </label> <br>
+          <small class='marL20'>${TRANS_QUERIES}: ${v.available_queries} / ${v.query_quantity} </small>
+          <br><small class='marL20'>${TRANS_VALIDITY_MONTHS}: ${v.validity_months} </small> <br>`;
+          
+          $('#change-chosen-list').append("<div class='container-modal-inputs'>"+html+text+"</div>");
+        });
+      }
+
+    // win.sendAjaxPagination(function(resp){
+    //     console.log(resp);
+    // });
 });
