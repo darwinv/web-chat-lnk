2dd8061ea1a2607269fe8b5836d66d3b4ce49ebd
darwinv <darwin.vasqz@gmail.com>
2018-11-14 09:00:57 -0500

    empieza detalle

--- frontend/views/match.py
+++ frontend/views/match.py
@@ -7,14 +7,23 @@ from django.contrib.auth.decorators import user_passes_test
 
 class Client:
     def __init__(self):
-        self.url_client = 'client/matchs/'
+        self.url_client_list = 'client/matchs/'
+        self.url_client_detail = 'match/'
 
     @method_decorator(user_passes_test(role_client_check()))
     def list_match(self, request):
         """Listado de Matchs."""
         obj_api = api()
         token = request.session['token']
-        data_matchs = obj_api.get(slug=self.url_client, token=token)
+        data_matchs = obj_api.get(slug=self.url_client_list, token=token)
         if data_matchs:
             match_list = sorted(data_matchs["results"], key=itemgetter('id'))
-        return render(request, 'frontend/actors/client/match.html', {'match_list': match_list})
+        return render(request, 'frontend/actors/client/match_list.html', {'match_list': match_list})
+
+    @method_decorator(user_passes_test(role_client_check()))
+    def detail_match(self, request, pk):
+        """Detalle de Match."""
+        obj_api = api()
+        token = request.session['token']
+        data_match = obj_api.get(slug=self.url_client_detail + pk, token=token)
+        return render(request, 'frontend/actors/client/match_detail.html', {'match': data_match})       
