b6553cff7f1eb252e3b2604f95695b6917e7e631
oscarlopezdeveloper <olopez@pympack.com.pe>
2018-11-20 09:14:38 -0500

    staticos

--- frontend/views/purchase.py
+++ frontend/views/purchase.py
@@ -1,22 +1,14 @@
 
-
+from django.shortcuts import render
 
 class Client:
-    def __init__(self):
-        self.url_client_list = 'client/matchs/'
-        self.url_client_detail = 'match/'
 
     @method_decorator(user_passes_test(role_client_check()))
-    def list_purchase_plans(self, request):
+    def list_purchase(self, request):
         """Listado de productos."""
         return render(request, 'frontend/actors/client/purchase_list.html')
 
     @method_decorator(user_passes_test(role_client_check()))
     def list_purchase_plans(self, request):
         """Listado de planes."""
-        obj_api = api()
-        token = request.session['token']
-        data_matchs = obj_api.get(slug=self.url_client_list, token=token)
-        if data_matchs:
-            match_list = sorted(data_matchs["results"], key=itemgetter('id'))
-        return render(request, 'frontend/actors/client/match_list.html', {'match_list': match_list})
+        pass
---
+++ static/frontend/dist/js/upload_voucher.js
@@ -0,0 +1,48 @@
+
+$(function() {
+    const HTTP_CODES = {OK:"200", CREATED:"201", BAD_REQUEST:"400", NOT_FOUND:"404"}
+
+    $(document).ready(function() {
+        $('#send-button').prop('disabled', true);
+    });
+
+    $('#voucher-section').click(function(event) {
+        $('#file-input').trigger('click');
+    });
+
+    $('#file-input').change(function(event) {
+        var file = $('#file-input').prop('files')[0];
+        if (file == null || file == "") {
+            return;
+        }
+
+        var preview = $('#voucher-img')
+        var reader  = new FileReader();
+
+        reader.onloadend = function () {
+           preview.attr('src', reader.result);
+           $('#send-button').prop('disabled', false);
+        }
+
+        reader.readAsDataURL(file);
+    });
+
+    $('#upload-voucher-form').on('submit', function(event) {
+        event.preventDefault();
+        upload();
+    });
+
+    function upload() {
+        var formData = new FormData(document.getElementById('upload-voucher-form'));
+        formData.append('file', $('#file-input').prop('files')[0]);
+        formData.append('url', upload_url);
+        formData.append('use_method', 'PUT');
+
+        uploadFileAjax(formData, function(response) {
+            if (response.status_code === HTTP_CODES.OK) {
+                $('#upload-voucher-modal').modal('hide');
+                $('#thanks-for-payment-modal').modal('show');
+            }
+        });
+    }
+});
