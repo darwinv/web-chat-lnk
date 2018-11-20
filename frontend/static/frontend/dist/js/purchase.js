169aba61822586de7c63214ee2f7b1dd91b0f816
darwinv <darwin.vasqz@gmail.com>
2018-11-20 18:47:26 -0500

    avance en ver productos

--- frontend/static/frontend/dist/js/purchase.js
+++ frontend/static/frontend/dist/js/purchase.js
@@ -23,4 +23,12 @@ $(function() {
         }
     });
 
+
+    $("#next").click(function(){
+        $("#plan-list").toggleClass("hidden");
+    });
+
+    $(".nav-tabs a").click(function(){
+        $(this).tab('show');
+    });
 });
