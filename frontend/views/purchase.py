c71e8ac4adbb562e50c06ab88f7362792e52e932
darwinv <darwin.vasqz@gmail.com>
2018-11-21 19:14:42 -0500

    se pasan lo datos para productos en resumen de compra

--- frontend/views/purchase.py
+++ frontend/views/purchase.py
@@ -19,4 +19,36 @@ class Client:
 
         plans = obj_api.get(slug='plans/', token=token)
         return render(request, 'frontend/actors/client/purchase_plan_list.html',
-                        {"plans":plans}
+                        {"plans":plans})
+
+
+    @method_decorator(user_passes_test(role_client_check()))
+    def summary_purchase_plans(self, request):
+        """Resumen compra de planes."""
+        obj_api = api()
+        token = request.session['token']
+        plans_purchase = []
+        products = []
+        ids_purchase = []
+        plans = obj_api.get(slug='plans/', token=token)
+        ids_plans = request.POST.getlist('vehicle')
+        import pdb; pdb.set_trace()
+        total = request.POST["total"]
+
+        for i in range(0, len(ids_plans)): 
+            id_p = int(ids_plans[i]) - 1
+            plans_purchase.append(plans[id_p])
+        
+        for plan in plans_purchase:
+            ids_purchase.append(plan['id'])
+            lines = [
+                plan['name'],
+                str(plan['query_quantity']) + " queries",
+                "Validty " + str(plan['validity_months']) + " months",
+                "S/. " + str(plan['price'])
+            ]
+            products.append({'lines':lines})
+        print(products)    
+
+        return render(request, 'frontend/actors/client/summary_plans.html',
+                        {"products":products, "total":total} )                
