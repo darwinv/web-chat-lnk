255b958702692dea16de5db6abec622f4b0cec20
darwinv <darwin.vasqz@gmail.com>
2018-11-22 15:28:10 -0500

    terminado lo de compra de productos

--- frontend/views/purchase.py
+++ frontend/views/purchase.py
@@ -3,6 +3,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 from api.connection import api
+import json  
 
 class Client:
 
@@ -33,21 +34,28 @@ class Client:
         plans = obj_api.get(slug='plans/', token=token)
         ids_plans = request.POST.getlist('vehicle')
         total = request.POST["total"]
+        is_fee = int(request.POST["modality"])
 
         for i in range(0, len(ids_plans)): 
             id_p = int(ids_plans[i]) - 1
             plans_purchase.append(plans[id_p])
         
-        for plan in plans_purchase:
-            ids_purchase.append(plan['id'])
-            lines = [
-                plan['name'],
-                str(plan['query_quantity']) + " queries",
-                "Validty " + str(plan['validity_months']) + " months",
-                "S/. " + str(plan['price'])
-            ]
-            products.append({'lines':lines})
-        print(products)    
 
+        for plan in plans_purchase:  
+            id_plan = plan["id"]      
+            plan_name = plan['name']
+            ids_purchase.append(id_plan)
+            total_queries = plan['query_quantity']
+            price = float(plan['price'])
+            validity = plan['validity_months']
+            if is_fee==1:
+                fee_queries = total_queries // validity
+                price /= validity
+            else:
+                fee_queries = None
+
+            products.append({'id':id_plan, 'name':plan_name, 'total_queries':total_queries,
+                            'fee_queries':fee_queries, 'validity':validity, 'price':price})     
+        # json_ids = json.loads(ids_purchase)
         return render(request, 'frontend/actors/client/summary_plans.html',
-                        {"products":products, "total":total} )                
+                        {"products":products, "total":total, "isfee":is_fee, "ids_plans":ids_purchase} )                
